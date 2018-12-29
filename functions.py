c0cc663f1b87e523736a57549d6f1980de840035
cesar <ocesaralmeida@gmail.com>
2018-12-29 21:21:24 -0200

    adding functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+'''
+def epar(x):
+    return x%2 == 0
+print(epar(13))
+'''
+def fat(n):
+    f = 1
+    while n > 0:
+        f = f * n
+        n = n - 1
+    return f
+
+for i in range(5):
+    print(fat(i))
