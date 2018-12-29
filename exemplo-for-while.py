f2d4e1f6d19213ec5a7476e899bd94da844d7849
cesar <ocesaralmeida@gmail.com>
2018-12-29 21:21:16 -0200

    adding exemplo-for-while.py

---
+++ exemplo-for-while.py
@@ -0,0 +1,30 @@
+'''
+for letra in 'aeiou':
+    print(letra)
+
+texto = 'aeiou'
+k = 0
+while k < len(texto):
+    letra = texto[k]
+    print(letra)
+    k += 1
+
+for i in range(5):
+    print(i)
+
+lista = list(range(5))
+k = 0
+while k < len(lista):
+    print(lista[k])
+    k += 1
+
+for x in ['cpbr6', 42, 3.14]:
+    print(x)
+
+lista = ['cpbr6', 42, 3.14]
+k = 0
+while k < len(lista):
+    x = lista[k]
+    print(x)
+    k += 1
+'''
