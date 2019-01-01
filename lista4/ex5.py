b8b78db0fb46eb8259b4c8afa8e29ea91ad49328
cesar <ocesaralmeida@gmail.com>
2019-01-01 14:53:17 -0200

    adding ex5.py to lista4

---
+++ lista4/ex5.py
@@ -0,0 +1,29 @@
+texto = "The Python Software Foundation and the global Python community \
+welcome and encourage participation by everyone. Our community is based on \
+mutual respect, tolerance, and encouragement, and we are working to help each \
+other live up to these principles. We want our community to be more diverse: \
+whoever you are, and whatever your background, we welcome you."
+
+resultado = []
+k = 0
+texto.split(' ')
+texto2 = ""
+while k < len(texto):
+    if texto[k] in ",.;/<>:?!@#$%&*()_+=-][{}´`~^ºª§\|°":
+        texto2 = texto2 + ""
+    else:
+        texto2 = texto2 + texto[k]
+    k += 1
+
+palavras = texto2.split(' ')
+prefixos = ["P","p","Y","y","T","t","H","h","O","o","N","n"]
+for x in palavras:
+    if len(x) > 3:
+        u = 0
+        for u in range(len(x)):
+            if u not in ("P","p","Y","y","T","t","H","h","O","o","N","n"):
+                if x not in resultado:
+                    resultado.append(x)
+        u +=1
+
+print('São, ' +str(len(resultado))+' palavras')
